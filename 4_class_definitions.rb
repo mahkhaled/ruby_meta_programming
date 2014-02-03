6c85e2ffa15177e632486a7c21b28f9de50c09b3
Mahmoud <mahmoud.khaled@badrit.com>
2014-02-03 20:41:43 +0200

    singleton method

--- 4_class_definitions.rb
+++ 4_class_definitions.rb
@@ -45,3 +45,39 @@ MyClass.read # => 1
 # Class variables are different from Class Instance Variables,
 # because they can be accessed by subclasses and by regu-lar instance methods. 
 
+# Singleton Method add method ONLY to a certain object not all class objects
+str = "just a regular string"
+def str.title?
+  self.upcase == self
+end
+str.title? # => false
+str.methods.grep(/title?/) # => ["title?"]
+str.singleton_methods # => ["title?"]
+
+
+class MyClass
+  puts self # => MyClass
+  # self.my_class_method is the same like singleton_method concept
+  # class method is a special kind for singleton method
+  def self.my_class_method
+    "class method"
+  end
+end
+
+# class macros
+class Class
+  def attr_accessor_methods(att)
+    define_method("get_#{att}") do
+      instance_variable_get("@#{att}".to_sym)
+    end
+  end
+end
+
+class MyClass
+  # attr_accessor_methods is a method called over receiver self (MyClass)
+  attr_accessor_methods :price
+end
+
+x = MyClass.new
+x.instance_variable_set(:@price, 10)
+x.get_price # => 10
